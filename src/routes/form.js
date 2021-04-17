7fcd0d1173aa15823d75181b8021b87e9adb76a2
miltondw <estradamilton2001@gmail.com>
2021-04-17 11:29:09 -0500

    cambio de heroku

--- src/routes/form.js
+++ src/routes/form.js
@@ -2,7 +2,7 @@ const { Router } =require('express')
 const router = Router()
 const { createForm,getForm} = require('../controllers/form.controller')
 
-router.route('https://miltondw.herokuapp.com/')
+router.route('/')
     .post(createForm)
     .get(getForm)
 
